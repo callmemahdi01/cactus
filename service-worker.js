83ad5eafa75dc5e19851d30ed2491d470967a05f
callmemahdi01 <mr1451493@gmail.com>
2024-07-17 19:28:01 +0330

    some changes

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,20 @@
 const CACHE_NAME = 'gpa-calculator-cache-v1';
 const urlsToCache = [
-    '/cactus/',
-    '/cactus/index.html',
-    '/cactus/style.css',
-    '/cactus/script.js',
-    '/cactus/reset.css',
-    '/cactus/Vazirmatn-font-face.css',
-    '/cactus/img/cactus-512x512.png',
-    '/cactus/img/cactus-192x192.png'
-  ];
+  '/cactus/',
+  '/cactus/index.html',
+  '/cactus/style.css',
+  '/cactus/script.js',
+  '/cactus/reset.css',
+  '/cactus/Vazirmatn-font-face.css',
+  '/cactus/img/cactus-512x512.png',
+  '/cactus/img/cactus-192x192.png'
+];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
@@ -23,21 +22,32 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     fetch(event.request)
       .then(response => {
+        // اگر پاسخ معتبر باشد، آن را در کش ذخیره می‌کنیم
         if (!response || response.status !== 200 || response.type !== 'basic') {
           return response;
         }
-        
-        const responseToCache = response.clone();
 
-        caches.open(CACHE_NAME)
-          .then(cache => {
-            cache.put(event.request, responseToCache);
-          });
+        const responseToCache = response.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, responseToCache);
+        });
 
         return response;
       })
-      .catch(() => caches.match(event.request))
+      .catch(() => {
+        // اگر اتصال به اینترنت وجود نداشته باشد، از کش استفاده می‌کنیم
+        return caches.match(event.request);
+      })
   );
+});
+
+// حذف کش‌ها در زمان آنلاین شدن
+self.addEventListener('online', () => {
+  caches.keys().then(cacheNames => {
+    cacheNames.forEach(cacheName => {
+      caches.delete(cacheName); // حذف تمام کش‌ها
+    });
+  });
 });
 
 self.addEventListener('activate', event => {
