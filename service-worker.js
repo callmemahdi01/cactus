9a89aaf64ce01e935bd0b03ed2ebbd8ed2ef7f83
callmemahdi01 <mr1451493@gmail.com>
2024-07-17 18:51:40 +0330

    pwa

---
+++ service-worker.js
@@ -0,0 +1,56 @@
+const CACHE_NAME = 'gpa-calculator-cache-v1';
+const urlsToCache = [
+    '/cactus/',
+    '/cactus/index.html',
+    '/cactus/style.css',
+    '/cactus/script.js',
+    '/cactus/reset.css',
+    '/cactus/Vazirmatn-font-face.css',
+    '/cactus/img/cactus-512x512.png',
+    '/cactus/img/cactus-192x192.png'
+  ];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        if (!response || response.status !== 200 || response.type !== 'basic') {
+          return response;
+        }
+        
+        const responseToCache = response.clone();
+
+        caches.open(CACHE_NAME)
+          .then(cache => {
+            cache.put(event.request, responseToCache);
+          });
+
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
