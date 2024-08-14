1d36d7e8a24d95f94ff8f76b10796c659b88990c
callmemahdi01 <mr1451493@gmail.com>
2024-08-14 16:56:27 +0330

    del about

--- about/script.js
+++
@@ -1,8 +0,0 @@
-function toggleList(id) {
-    const list = document.getElementById(id);
-    if (list.style.display === "none") {
-        list.style.display = "block";
-    } else {
-        list.style.display = "none";
-    }
-}
